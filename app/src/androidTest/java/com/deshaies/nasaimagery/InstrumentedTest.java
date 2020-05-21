4c193426924edd81de35421653e4aa7acf0326bf
Jean-Paul Deshaies <jdeshaies1982@gmail.com>
2020-05-21 16:54:04 -0400

    espresso instrumented test

--- app/src/androidTest/java/com/deshaies/nasaimagery/InstrumentedTest.java
+++ app/src/androidTest/java/com/deshaies/nasaimagery/InstrumentedTest.java
@@ -4,10 +4,17 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import com.deshaies.nasaimagery.view.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,12 +23,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class InstrumentedTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
         assertEquals("com.deshaies.nasaimagery", appContext.getPackageName());
+    }
+
+    @Test
+    public void swipeDownTest() {
+        onView(withId(R.id.swipe_refresh)).perform(swipeDown());
     }
 }
